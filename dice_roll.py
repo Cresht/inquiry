4cf9c5234ca9c8f04baeb15492dc187d22ac2464
Cresht <90884776+Cresht@users.noreply.github.com>
2021-09-23 20:46:32 -0500

    added my old dice roll function
    this function produces the sample space for n rolls of a fair die.

---
+++ dice_roll.py
@@ -0,0 +1,9 @@
+def foo(n, rolls=[], roll=[]):
+    if n > 0:
+        for i in range(1,7):
+            foo(n-1, rolls, roll+[i])
+    else:
+       rolls.append(roll)
+    return rolls
+
+print(rolls:=foo(3), "\n\n", [sum(roll) for roll in rolls])
